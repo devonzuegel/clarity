0258d13e2432147ffa3d38e8e0bd859ff51561bb
devon <devon@affirm.com>
2017-04-09 16:35:38 -0700

    Separate Webpack config for dev and prod

--- webpack/server/index.ts
+++ webpack/server/index.ts
@@ -3,13 +3,13 @@ import * as path from 'path'
 import { config } from './config'
 import * as Env   from '../models/Environment'
 
-const env: Env.Type = Env.Enum.development
+export const setup = (env: Env.Type) => {
+  const isProd = String(env) === String(Env.Enum.production)
 
-const isProd = String(env) === String(Env.Enum.production)
-
-export default config({
-  rootDir:   path.join(__dirname, '../..'),
-  outputDir: isProd ? 'dist' : 'build',
-  devtool:   'sourcemap',
-  console:   !isProd,
-})
+  return config({
+    rootDir:   path.join(__dirname, '../..'),
+    outputDir: isProd ? 'dist' : 'build',
+    devtool:   'sourcemap',
+    console:   !isProd,
+  })
+}
