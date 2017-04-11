c055ad87370313ccd7a5244c0d28d01e8253276d
devon <devon@affirm.com>
2017-04-11 01:51:21 -0700

    Add toy <Counter /> with functional state

--- src/frontend/main.tsx
+++ src/frontend/main.tsx
@@ -1,9 +1,36 @@
 import * as React    from 'react'
 import * as ReactDOM from 'react-dom'
 
+interface IState {
+  count: number
+}
+
+const increment = (prevState: IState): IState => ({ count: prevState.count + 1 })
+const decrement = (prevState: IState): IState => ({ count: prevState.count - 1 })
+
+class Counter extends React.Component<null, IState> {
+  state = {count: 0}
+
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.setState(increment)}>
+          Increment
+        </button>
+
+        <b>
+          {this.state.count}
+        </b>
+
+        <button onClick={() => this.setState(decrement)}>
+          Decrement
+        </button>
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <h1>
-    Hello world!
-  </h1>,
+  <Counter />,
   document.getElementById('root'),
 )
