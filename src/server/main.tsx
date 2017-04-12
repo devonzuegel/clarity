5c545a990f4e621e5caf965a271376cbb6c95441
devon <devon@affirm.com>
2017-04-12 09:26:34 -0700

    Add Helmet middleware

--- src/server/main.tsx
+++ src/server/main.tsx
@@ -4,14 +4,20 @@ import { runHotMiddleware, listen } from './middleware'
 const config = require('./config.js')
 const app    = express()
 
+app.use(require('helmet')())
+
 if (config.env !== 'production') {
   runHotMiddleware(app)
 }
 
 app.use('/', express.static(__dirname))
 
-app.get('*', (_: express.Request, res: express.Response) => {
+app.get('/', (_: express.Request, res: express.Response) => {
   res.status(200).send(renderHTML())
+})
+
+app.get('/data', (_: express.Request, res: express.Response) => {
+  res.status(200).json([1, 2, 3])
 })
 
 listen(app, config)
