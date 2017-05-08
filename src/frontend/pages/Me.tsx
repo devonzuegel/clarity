f94295159dc5a4e0b5ffe5b628b8ae604486156e
devon <devon@affirm.com>
2017-05-07 21:37:38 -0700

    Retrieve all posts on the Me page

--- src/frontend/pages/Me.tsx
+++ src/frontend/pages/Me.tsx
@@ -1,9 +1,36 @@
 import * as React from 'react'
 
+import {get, sendRequest} from '../../../utils/api/responses'
+import {PostAttributes} from '../../server/db/models/post'
+
 import Layout from '~/frontend/stories/Layout'
 
-export default () => {
-  return (
-    <Layout />
-  )
+interface IState {posts?: Object[]}
+
+class Me extends React.Component<{}, IState> {
+  state = {posts: undefined}
+
+  updatePostsList = (posts: PostAttributes[]) => (_: IState) => ({posts})
+
+  componentWillMount () {
+    sendRequest(get('/api/posts')).then((posts: PostAttributes[]) => {
+      this.setState(this.updatePostsList(posts))
+    })
+  }
+
+  render () {
+    return (
+      <Layout>
+        <pre>
+          {
+            this.state.posts
+            ? JSON.stringify(this.state.posts, null, 2)
+            : 'Retrieving posts...'
+          }
+        </pre>
+      </Layout>
+    )
+  }
 }
+
+export default Me
