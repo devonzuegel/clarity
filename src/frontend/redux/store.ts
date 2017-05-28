04279c6cf7a4b8ac7c09d26c9e93540d71d983cf
devon <devon@affirm.com>
2017-05-28 02:58:25 -0700

    Add Redux Devtools

--- src/frontend/redux/store.ts
+++ src/frontend/redux/store.ts
@@ -1,8 +1,15 @@
 import * as Redux  from 'redux'
 import {rootReducer} from './reducers'
 
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION__: Function,
+  }
+}
+
 export const configureStore = (): Redux.Store<any> => {
-  const store = Redux.createStore(rootReducer)
+  const devtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  const store = Redux.createStore(rootReducer, devtools)
 
   if (module.hot) {
     (module as any).hot.accept('./reducers', () => {
