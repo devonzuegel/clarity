747cb3b208ab3ef04fb46d97aae041e50c58c19b
devon <devon@affirm.com>
2017-05-29 18:44:13 -0700

    Set frontend/main.tsx as hot middleware entry for better performance

--- src/server/middleware.ts
+++ src/server/middleware.ts
@@ -3,7 +3,10 @@ import * as express from 'express'
 export const runHotMiddleware = (app: express.Application) => {
   const webpack         = require('webpack')
   const webpackConfig   = require('../../webpack/frontend/development').default
-  const webpackCompiler = webpack(webpackConfig)
+  const webpackCompiler = webpack({
+    ...webpackConfig,
+    entry: './src/frontend/main.tsx',
+  })
 
   app.use(require('webpack-dev-middleware')(webpackCompiler, {
     publicPath:         webpackConfig.output.publicPath,
