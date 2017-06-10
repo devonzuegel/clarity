fe184d55bb7bea65465acbb049b0b04eea858473
devon <devon@affirm.com>
2017-06-09 23:50:05 -0700

    Fix functional/posts/new FT

--- test/functional/posts/new.js
+++ test/functional/posts/new.js
@@ -4,7 +4,7 @@ const {randomStr} = require('../utils')
 const selectors = {
   lastPost:   'div[id="posts-list"] > div:last-child .post-list--post-title a',
   createBtn:  'button#post-form--create__create-button',
-  iterateBtn: 'button#posts--show--iterate-btn',
+  iterateBtn: '#posts--show--iterate-btn',
   reviseBtn:  'button#post-form--revise__create-button',
   titleInput: 'input#post-form--revise__title',
 }
@@ -26,7 +26,7 @@ module.exports = {
     const title2   = 'New title'
     const content2 = 'New content'
     browser
-      // Create Post
+      /** Create Post **/
       .url(`${url}/posts/new`)
       .setValue('input#post-form--create__title', title1)
       .waitForElementVisible(selectors.createBtn, 1000)
@@ -34,23 +34,26 @@ module.exports = {
       .url(`${url}/posts`)
       .waitForElementVisible('.post-list--post-title', 1000)
       .assert.containsText(selectors.lastPost, title1)
-      // Create Iteration on Post
+      /** Create Iteration on Post **/
       .click(selectors.lastPost)
+      .pause(100)
       .waitForElementVisible(selectors.iterateBtn, 1000)
       .click(selectors.iterateBtn)
       .keys(content2)
+      .waitForElementVisible(selectors.titleInput, 1000)
       .clearValue(selectors.titleInput)
       .setValue(selectors.titleInput, title2)
-      .assert.elementPresent('#iteration-0')
+      .assert.elementNotPresent('#iteration-0') // Hidden until more than one
       .assert.elementNotPresent('#iteration-1')
       .click(selectors.reviseBtn)
-      // Assert that a new iteration was added to the timeline
-      .waitForElementVisible('#iteration-1', 1000)
+      /** Assert that a new iteration was added to the timeline **/
+      .waitForElementVisible('#iteration-0', 1000)
+      .assert.elementPresent('#iteration-0')
       .assert.elementPresent('#iteration-1')
-      .click('button#iteration-1')
+      .click('#iteration-1')
       .assert.containsText('#iteration-title', title2)
       .assert.containsText('#iteration-body', content2)
-      .click('button#iteration-0')
+      .click('#iteration-0')
       .assert.containsText('#iteration-title', title1)
   },
   after: browser => browser.end(),
