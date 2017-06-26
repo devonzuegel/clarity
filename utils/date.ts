a569d65de677ccbdb8ba920791ab501595edeeec
devon <devon@affirm.com>
2017-06-25 17:02:23 -0700

    Implement utils#formatDate

--- utils/date.ts
+++ utils/date.ts
@@ -1,7 +1,18 @@
+import * as moment from 'moment'
+
 export const formatDateStr = (s: string) => {
-  const result = new Date(Date.parse(s)).toLocaleString('en-US', { hour12: false })
+  const result = new Date(Date.parse(s)).toLocaleTimeString('en-UK', {
+    hour12: false,
+    day: '2-digit',
+    month: 'numeric',
+    year: 'numeric',
+  })
   if (result === 'Invalid Date') {
     throw Error(result)
   }
   return result
+}
+
+export const formatDate = (d: Date) => {
+  return moment(d).format('DD-MM-YYYY HH:mm:ss')
 }
--- utils/date.ut.ts
+++ utils/date.ut.ts
@@ -8,9 +8,9 @@ describe('formatDateStr', () => {
   })
   it('handles valid date strings', () => {
     const examples = [
-      ['June 11 2012',            '6/11/2012, 00:00:00'],
-      ['11 June 2012',            '6/11/2012, 00:00:00'],
-      ['2017-05-29 19:48:29.289', '5/29/2017, 19:48:29' ],
+      ['June 11 2012', '6/11/2012, 00:00:00'],
+      ['11 June 2012', '6/11/2012, 00:00:00'],
+      ['2017-05-29 19:48:29.289', '5/29/2017, 19:48:29'],
     ]
     examples.map(([input, expected]: string[]) => {
       const output = U.formatDateStr(input)
@@ -19,3 +19,15 @@ describe('formatDateStr', () => {
   })
 })
 
+describe('formatDate', () => {
+  it('handles valid date strings', () => {
+    const examples = [
+      [new Date('May 1 2016'), '5/01/2016, 00:00:00'],
+      [new Date('Dec 31 2019'), '12/31/2019, 00:00:00'],
+    ]
+    examples.map(([input, expected]: string[]) => {
+      const output = U.formatDateStr(input)
+      expect(output).toEqual(expected)
+    })
+  })
+})
