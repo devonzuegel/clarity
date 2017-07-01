b0cdf7a0ad1065218b36a685a4459efaa6446a30
devon <devon@affirm.com>
2017-07-01 14:52:11 -0700

    Add WARNING when NODE_ENV conflicts with .env

--- src/server/config/environments.ts
+++ src/server/config/environments.ts
@@ -1,21 +1,41 @@
+const R = require('ramda')
 const path = require('path')
 const chalk = require('chalk')
 const dotenv = require('dotenv')
+const envFile = '.env'
 
 const getEnvVariables = () => {
-  const fromDotenv = dotenv.config({path: path.resolve('.env')})
+  const fromDotenv = dotenv.config({path: path.resolve(envFile)})
+
   if (fromDotenv.error) {
-    console.warn(chalk.yellow(`No config file was found at ${fromDotenv.error.path}`))
+    console.warn(
+      chalk.yellow(`No config file was found at ${fromDotenv.error.path}`)
+    )
     return process.env
   }
+
+  if (fromDotenv.parsed.NODE_ENV !== process.env.NODE_ENV) {
+    console.info(
+      chalk.yellow(
+        `WARNING: ${envFile} specifies NODE_ENV=${fromDotenv.parsed.NODE_ENV}, ` +
+          `but it be overriden by NODE_ENV=${process.env.NODE_ENV}`
+      )
+    )
+  }
+
+  const result = R.merge(fromDotenv.parsed, {
+    NODE_ENV: process.env.NODE_ENV, // Override
+  })
+
   console.info(chalk.green('Environment variables loaded from .env:'))
-  console.info(chalk.grey(JSON.stringify(fromDotenv, null, 2)))
-  return fromDotenv.parsed
+  console.info(chalk.grey(JSON.stringify(result, null, 2)))
+
+  return result
 }
 
 module.exports = {
   HEROKU_ENVS: ['heroku-develop', 'heroku-stage', 'heroku-live'],
-  LOCAL_ENVS: ['local-develop'],
+  LOCAL_ENVS: ['test', 'local-develop'],
   TEST_ENVS: ['test', 'ci'],
   getEnvVariables,
 }
--- src/server/http/authentication/index.ts
+++ src/server/http/authentication/index.ts
@@ -1,8 +1,11 @@
+import * as R from 'ramda'
 import * as express from 'express'
 import * as passport from 'passport'
 
 import {IPassportConfig, setupStrategy} from '~/server/service/authentication'
 import * as Facebook from '~/server/http/authentication/facebook'
+const {TEST_ENVS} = require('~/server/config/environments')
+const {env} = require('~/server/config/index.js')
 
 export const setup = (config: IPassportConfig) => (app: express.Application) => {
   setupStrategy(config)
@@ -10,5 +13,9 @@ export const setup = (config: IPassportConfig) => (app: express.Application) => 
   app.use(passport.initialize())
   app.use(passport.session())
 
-  Facebook.setup(app)
+  if (R.contains(env, TEST_ENVS)) {
+    Facebook.setup(app)
+  } else {
+    Facebook.setup(app)
+  }
 }
