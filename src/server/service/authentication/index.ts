1a71fbd8648d6487430a53842791246fa0d147a1
devon <devon@affirm.com>
2017-07-01 14:17:20 -0700

    Create directory for authentication service

--- src/server/service/authentication/index.ts
+++ src/server/service/authentication/index.ts
@@ -3,6 +3,7 @@ import * as Facebook from 'passport-facebook'
 
 import {userService} from '~/server/service/user'
 import {UserInstance} from '~/server/db/models/user'
+const Mock = require('~/server/service/authentication/strategy.mock')
 
 export interface IPassportConfig {
   clientID: string
@@ -19,6 +20,20 @@ export const setupStrategy = (c: IPassportConfig) => {
         .then((_u: UserInstance) => done(null, profile))
         .catch((e: any) => done(JSON.stringify(e)))
     })
+  )
+
+  passport.use(
+    // TODO: any
+    new Mock.Strategy(
+      c,
+      (_token: string, _refreshToken: string, profile: any, done: Function) => {
+        const facebookId = profile.id
+        userService
+          .signIn(facebookId)
+          .then((_u: UserInstance) => done(null, profile))
+          .catch((e: any) => done(JSON.stringify(e)))
+      }
+    )
   )
 
   passport.serializeUser((user, done) => done(null, user))
