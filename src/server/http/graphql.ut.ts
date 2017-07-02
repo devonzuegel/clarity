3aa2ac4deb0f4aa577f8bf48d2dfd2ca0f997428
devon <devon@affirm.com>
2017-07-02 01:31:10 -0700

    Extract GraphQL books

--- src/server/http/graphql.ut.ts
+++ src/server/http/graphql.ut.ts
@@ -2,17 +2,15 @@ import {getWithData} from '~/../utils/http/test'
 import {newApp} from '~/../utils/http/newApp'
 
 import GraphqlAPI from '~/server/http/graphql'
-import {mockBooks} from '~/server/graphql/schema'
+import {mockBooks} from '~/server/graphql/books'
 
 const app = newApp([GraphqlAPI])
 
 describe('GraphQL API', () => {
-  describe('/api/users/create', () => {
-    it('returns a created user', done => {
-      getWithData(app, '/graphql', {query: '{books{id,title}}'}, res => {
-        expect(res.body).toEqual({data: {books: mockBooks}})
-        done()
-      })
+  it('gets the dummy book data', done => {
+    getWithData(app, '/graphql', {query: '{books{id,title}}'}, res => {
+      expect(res.body).toEqual({data: {books: mockBooks}})
+      done()
     })
   })
 })
