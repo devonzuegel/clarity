3d90ef25d31fd9994537f34bee22eb2243bfb3ff
devon <devon@affirm.com>
2017-07-01 22:37:59 -0700

    Add FT for creating a new post

--- test/functional/config.js
+++ test/functional/config.js
@@ -6,9 +6,25 @@ if (!env) {
   throw Error('Please define NODE_ENV')
 }
 
+const url = env === 'ci'
+  ? 'http://clarity-tests.herokuapp.com'
+  : `http://${host}:${port}`
+
+const logIn = browser =>
+  browser
+    .url(`${url}/posts`)
+    .click(nav.logIn) // App must be running in a test environment
+    .waitForElementVisible('#root', 1000)
+
+const nav = {
+  logIn: 'a#nav--log-in',
+  posts: 'a#nav--document',
+  newPost: 'a#nav--plus',
+}
+
 module.exports = {
   ci: env === 'ci',
-  url: env === 'ci'
-    ? 'http://clarity-tests.herokuapp.com'
-    : `http://${host}:${port}`,
+  url,
+  logIn,
+  nav,
 }
--- test/functional/posts/new.js
+++ test/functional/posts/new.js
@@ -1,50 +1,65 @@
-const {url}       = require('../config')
+const {url, logIn, nav} = require('../config')
 const {randomStr} = require('../utils')
 
 const selectors = {
-  lastPost:   'div[id="posts-list"] > div:last-child .post-list--post-title a',
-  createBtn:  'button#post-form--create__create-button',
+  nav,
+  lastPost: 'div[id="posts-list"] > div:last-child .post-list--post-title a',
+  createBtn: 'button#post-form--create__create-button',
   iterateBtn: '#posts--show--iterate-btn',
-  reviseBtn:  'button#post-form--revise__create-button',
+  reviseBtn: 'button#post-form--revise__create-button',
   titleInput: 'input#post-form--revise__title',
+  contentInput: '.CodeMirror',
 }
 
 module.exports = {
-  // // TODO
-  // 'Create a new post, then iterate': browser => {
-  //   const title1   = randomStr()
-  //   const title2   = 'New title'
-  //   const content2 = 'New content'
-  //   browser
-  //     /** Create Post **/
-  //     .url(`${url}/posts/new`)
-  //     .setValue('input#post-form--create__title', title1)
-  //     .waitForElementVisible(selectors.createBtn, 1000)
-  //     .click(selectors.createBtn)
-  //     .url(`${url}/posts`)
-  //     .waitForElementVisible('.post-list--post-title', 1000)
-  //     .assert.containsText(selectors.lastPost, title1)
-  //     /** Create Iteration on Post **/
-  //     .click(selectors.lastPost)
-  //     .pause(100)
-  //     .waitForElementVisible(selectors.iterateBtn, 1000)
-  //     .click(selectors.iterateBtn)
-  //     .keys(content2)
-  //     .waitForElementVisible(selectors.titleInput, 1000)
-  //     .clearValue(selectors.titleInput)
-  //     .setValue(selectors.titleInput, title2)
-  //     .assert.elementNotPresent('#iteration-0') // Hidden until more than one
-  //     .assert.elementNotPresent('#iteration-1')
-  //     .click(selectors.reviseBtn)
-  //     /** Assert that a new iteration was added to the timeline **/
-  //     .waitForElementVisible('#iteration-0', 1000)
-  //     .assert.elementPresent('#iteration-0')
-  //     .assert.elementPresent('#iteration-1')
-  //     .click('#iteration-1')
-  //     .assert.containsText('#iteration-title', title2)
-  //     .assert.containsText('#iteration-body', content2)
-  //     .click('#iteration-0')
-  //     .assert.containsText('#iteration-title', title1)
-  // },
+  'Create a new post, then iterate': browser => {
+    const title1 = randomStr()
+    const title2 = 'New title'
+    const content2 = 'New content'
+    logIn(browser)
+      /*****************************************************/
+      /*** Sign in *****************************************/
+      /*****************************************************/
+      .click(selectors.nav.newPost)
+      /*****************************************************/
+      /*** Create Post *************************************/
+      /*****************************************************/
+      // Set title
+      .setValue('input#post-form--create__title', title1)
+      // Set content
+      .click(selectors.contentInput)
+      .keys('foo bar baz qux')
+      // Click button
+      .waitForElementVisible(selectors.createBtn, 1000)
+      .click(selectors.createBtn)
+      // Check posts
+      // .click(selectors.nav.posts)
+      .waitForElementVisible('.post-list--post-title', 1000)
+      .assert.containsText(selectors.lastPost, title1)
+      /*****************************************************/
+      /** Create Iteration on Post *************************/
+      /*****************************************************/
+      .click(selectors.lastPost)
+      .waitForElementVisible(selectors.iterateBtn, 1000)
+      .click(selectors.iterateBtn)
+      .keys(content2)
+      .waitForElementVisible(selectors.titleInput, 1000)
+      .clearValue(selectors.titleInput)
+      .setValue(selectors.titleInput, title2)
+      .assert.elementNotPresent('#iteration-0') // Hidden until more than one
+      .assert.elementNotPresent('#iteration-1')
+      .click(selectors.reviseBtn)
+      /*****************************************************/
+      /** Assert that a new iteration was added ************/
+      /*****************************************************/
+      .waitForElementVisible('#iteration-0', 1000)
+      .assert.elementPresent('#iteration-0')
+      .assert.elementPresent('#iteration-1')
+      .click('#iteration-1')
+      .assert.containsText('#iteration-title', title2)
+      .assert.containsText('#iteration-body', content2)
+      .click('#iteration-0')
+      .assert.containsText('#iteration-title', title1)
+  },
   after: browser => browser.end(),
-};
+}
