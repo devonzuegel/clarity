b650ab8bb616d70729202e18b4425d1fc8b6e4c3
John Backus <johncbackus@gmail.com>
2017-07-04 00:22:05 -0700

    Refactor date functions to use Ramda

--- utils/date.ts
+++ utils/date.ts
@@ -1,4 +1,5 @@
 import * as format from 'date-fns/format'
+import * as R from 'ramda'
 
 export const formatDateStr = (s: string) => {
   const result = new Date(Date.parse(s)).toLocaleTimeString('en-UK', {
@@ -13,10 +14,5 @@ export const formatDateStr = (s: string) => {
   return result
 }
 
-export const formatDate = (d: Date) => {
-  return format(d, 'DD-MM-YYYY HH:mm:ss')
-}
-
-export const formatDateLong = (d: Date) => {
-  return format(d, 'DD MMM YYYY, HH:mm')
-}
+export const formatDate = R.partialRight(format, ['DD-MM-YYYY HH:mm:ss'])
+export const formatDateLong = R.partialRight(format, ['DD MMM YYYY, HH:mm'])
