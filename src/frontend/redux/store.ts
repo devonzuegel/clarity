42b19de64a18a7711da2c44a6fa96c7e26e09201
devon <devon@affirm.com>
2017-08-03 22:13:46 -0700

    Persist Redux state to LocalStorage

--- src/frontend/redux/store.ts
+++ src/frontend/redux/store.ts
@@ -1,4 +1,5 @@
 import * as Redux from 'redux'
+import * as persistState from 'redux-localstorage'
 
 import Hermes from '~/../utils/hermes'
 import {rootReducer, rootReducerMock} from '~/frontend/redux/reducers'
@@ -15,15 +16,20 @@ const logger = new Hermes({name: 'frontend'})
 const configureStore = (): Redux.Store<any> => {
   const devtools = window.__REDUX_DEVTOOLS_EXTENSION__
   const _m = module as any
+  const enhancer = Redux.compose((persistState as any)())
 
   if (_m.hot && useMock) {
     logger.info('Loading rootReducerMock')
-    const store = Redux.createStore(rootReducerMock, devtools && devtools())
+    const store = Redux.createStore(
+      rootReducerMock,
+      devtools && devtools(),
+      enhancer
+    )
     _m.hot.accept('./reducers', () => store.replaceReducer(require('./reducers')))
     return store
   }
 
-  const store = Redux.createStore(rootReducer, devtools && devtools())
+  const store = Redux.createStore(rootReducer, devtools && devtools(), enhancer)
   return store
 }
 
