f5af6a92935bd77e13b099d14c584912dcd1b4de
devon <devon@affirm.com>
2017-08-04 20:50:47 -0700

    Remove the app-oriented nav bar when not signed in

--- src/frontend/components/Layout/index.tsx
+++ src/frontend/components/Layout/index.tsx
@@ -15,31 +15,21 @@ const NavBtn = ({url, title, name}: {url: string; title: string; name: string}) 
     </button>
   </a>
 
-const Brand = () =>
-  <div className="pt-navbar-group pt-align-left">
-    <div className="pt-navbar-heading">
-      Clarity
-    </div>
-  </div>
-
 const LayoutComponent = ({displayName, username, children}: ILayout) =>
   <div>
     <nav style={{display: 'flow-root', height: '48px', marginBottom: '25px'}}>
-      <Brand />
-      <div className="pt-navbar-group pt-align-right">
-        {displayName &&
+      {displayName &&
+        <div className="pt-navbar-group pt-align-right">
           <div>
             <NavBtn title="New post" url={urls.newPost} name="plus" />
             <NavBtn title={displayName} url={urls.user(username)} name="user" />
             <span className="pt-navbar-divider" />
-          </div>}
-        {displayName
-          ? <NavBtn title="Sign out" url={urls.signout} name="log-out" />
-          : <NavBtn title="Sign in" url={urls.fbSignin} name="log-in" />}
-      </div>
+          </div>
+          <NavBtn title="Sign out" url={urls.signout} name="log-out" />
+        </div>}
     </nav>
 
-    <main>
+    <main style={{paddingBottom: '150px'}}>
       {children}
     </main>
   </div>
