dbc355df8b95e3c8a1079b982d8f6b25ccaea0f9
devon <devon@affirm.com>
2017-08-15 12:50:30 -0700

    test: Add FT for navigating to user profile

---
+++ test/specs/authentication/userProfile.js
@@ -0,0 +1,16 @@
+const {nav} = require('../../config')
+const {logIn, randomStr} = require('../../utils')
+
+module.exports = {
+  'The user navigates to her profile from the logged-in nav bar': browser => {
+    const username = randomStr()
+    logIn(browser, username)
+      .click(nav.userProfile)
+      .url(function(result) {
+        this.assert.ok(result.value.endsWith(`/@${username}`))
+      })
+      .assert.containsText('main', 'No posts to display')
+  },
+
+  after: browser => browser.end(),
+}
